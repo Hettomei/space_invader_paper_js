99627a4ca18ce2b04e76c846a9e1f7e224373e22
Hettomei <itsumo.sibyllin@gmail.com>
2013-10-14 11:55:17 +0200

    Control ship with mouse. + add one alien

--- canvas.js
+++ canvas.js
@@ -3,35 +3,43 @@ val = {
   canvas_y: 480,
 };
 
-
 view.viewSize =  new Size(val.canvas_x, val.canvas_y);
 
-
-
 var ship = new Ship();
-var fire_controller = new FireController();
+var fire_controller = new FireController(ship);
 ship.position = new Point(val.canvas_x / 2, val.canvas_y - 15);
 
+var alien = new Alien();
 /* event */
+function onMouseMove(event) {
+  // Whenever the user moves the mouse, move the path
+  // to that position:
+  ship.position.x = event.point.x;
+  if(ship.position.x < 20){
+    ship.position.x = 20;
+  }else if(ship.position.x > val.canvas_x - 20){
+    ship.position.x = val.canvas_x - 20;
+  }
+}
+
+function onMouseUp(event) {
+  fire_controller.new_fire();
+}
+
 function onKeyDown(event) {
   if (event.key == 'space') {
-
-    var fire = new Fire();
-    fire.position = ship.position;
-    fire_controller.add(fire);
-
-    // Prevent the key event from bubbling
+    fire_controller.new_fire();
   }
 
   if (event.key == 'left') {
-
     ship.position += new Point(-5, 0);
+  }
 
-    // Prevent the key event from bubbling
-  }else if (event.key == 'right') {
+  if (event.key == 'right') {
     ship.position += new Point(5, 0);
   }
 
+  // Prevent the key event from bubbling
   return false;
 }
 
@@ -68,15 +76,49 @@ function Ship(){
   return this.ship;
 }
 
+function Alien(){
+  this.alien = new Path();
+
+  this.alien.strokeColor = 'green';
+
+  this.alien.add(new Point(0, 10));
+  this.alien.add(new Point(4, 0));
+  this.alien.add(new Point(8, 10));
+  this.alien.add(new Point(6, 20));
+  this.alien.add(new Point(4, 10));
+  this.alien.add(new Point(2, 20));
+  this.alien.closed = true;
+
+  return this.alien;
+}
+
 /* Controller */
-function FireController(){
+function FireController(ship){
   this.list = [];
+  this.ship = ship;
+  this.LEFT = 0;
+  this.RIGHT = 1;
+  this.side = this.LEFT;
+
   this.move_all = function(){
     for(var i = 0; i < this.list.length; i++){
-      this.list[i].position += new Point(0, -5);
+      this.list[i].position += new Point(0, -10);
     }
   };
+
   this.add = function(fire){
     this.list.push(fire);
   };
+
+  this.new_fire = function(){
+    var fire = new Fire();
+    if(this.side === this.LEFT){
+      fire.position = this.ship.position + new Point(-10, -10);
+      this.side = this.RIGHT;
+    }else{
+      fire.position = this.ship.position + new Point(10, -10);
+      this.side = this.LEFT;
+    }
+    this.add(fire);
+  }
 }
